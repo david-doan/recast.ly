3faf9b1b0c920fe74288872203f9ec7b5322f84a
david-doan <a.david.doan@gmail.com>
2016-05-06 19:33:12 -0700

    completes searchYouTube.js

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,26 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  var urlStr = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
+                    + `&maxResults=${options.max}` 
+                    + '&type=video'
+                    + '&videoEmbeddable=true'
+                    + `&q=${options.query}`
+                    + `&key=${options.key}`;
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: urlStr,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('youTube: Data received');
+      console.log(data);
+      callback(data.items);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('youTube: Failed to get data', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
