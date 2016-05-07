c3b25e1185f743df67ef0ea6b2afd5fde1fbbcb3
david-doan <a.david.doan@gmail.com>
2016-05-06 18:21:29 -0700

    completes hook of video list and video player components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,38 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      currVideo: window.exampleVideoData[0],
+      videoList: window.exampleVideoData
+    };
+  }
+
+  videoClick(video) {
+    this.setState({
+      currVideo: video
+    });
+    this.render();
+  }
+
+  flagProperty(){
+    console.log('hello');
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video = {this.state.currVideo} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos = {this.state.videoList} videoClick = {this.videoClick.bind(this)} />
+        </div>
+      </div>  
+    );  
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
